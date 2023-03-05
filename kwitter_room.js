070a671fad102c4c499848dcddfdd4305c74ff64
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-03-05 15:56:55 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,10 +1,26 @@
 
 //ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+  apiKey: "AIzaSyAZcb3Fjrsh0ybOOXC6ZkNBRFucbZBepSU",
+  authDomain: "twitter2-d4b6d.firebaseapp.com",
+  databaseURL: "https://twitter2-d4b6d-default-rtdb.firebaseio.com",
+  projectId: "twitter2-d4b6d",
+  storageBucket: "twitter2-d4b6d.appspot.com",
+  messagingSenderId: "290799928667",
+  appId: "1:290799928667:web:8959d0c02c19d59ccc867e"
+};
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
-      //End code
-      });});}
-getData();
+    function userNamez() {
+      var user_Name =  localStorage.getItem("user")
+      firebase.database().ref("/").child(localStorage.getItem("user")).update({
+        USER: user_Name
+   });
+ }
+
+ userNamez()
+
+
+
