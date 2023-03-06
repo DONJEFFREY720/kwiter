fee05d7cee5a832578df23bf893b8f7f3d45eb84
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-03-06 19:40:24 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -12,6 +12,8 @@ var firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
 Room_names = childKey;
 
     function userNamez() {
